d52167c50c68ba9ae41de75c9184afcb6fcd8d85
CamEstep <camestep8517@gmail.com>
2022-07-21 17:19:20 -0400

    Added lot of forms and such.

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -34,22 +34,22 @@ def dashboard(request):
 @login_required
 def edit(request):
   if request.method == 'POST':
-    user_form = UserEditForm(instance=request.user,
-                              data=request.POST)
-    profile_form = ProfileEditForm(
+      user_form = UserEditForm(instance=request.user,
+                                data=request.POST)
+      profile_form = ProfileEditForm(
                                   instance=request.user.profile,
                                   data=request.POST,
                                   files=request.FILES)
-    if user_form.is_valid() and profile_form.is_valid():
-      user_form.save()
-      profile_form.save()
+      if user_form.is_valid() and profile_form.is_valid():
+          user_form.save()
+          profile_form.save()
   else:
-    user_form = UserEditForm(instance=request.user)
-    profile_form = ProfileEditForm(instance=request.user.profile)
+      user_form = UserEditForm(instance=request.user)
+      profile_form = ProfileEditForm(instance=request.user.profile)
   return render(request,
                 'account/edit.html',
-                {'user_form:': user_form,
-                  'profile_from': profile_form })
+                {'user_form': user_form,
+                  'profile_form': profile_form})
 
 def register(request):
   if request.method == 'POST':
